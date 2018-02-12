0213227425e0abc22649e6193e069c270e7dc734
Verwijnen Terhi K <trissa@lx7-fuxi048.ad.helsinki.fi>
2018-02-12 16:32:32 +0200

    päivitetty sqliteLauseita

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -42,8 +42,8 @@ public class Database {
         lista.add("CREATE TABLE Resepti (id integer PRIMARY KEY, nimi varchar(255), luokitus double, valmistusaika integer);");
         lista.add("CREATE TABLE Raaka_aine (id integer PRIMARY KEY, nimi varchar(255));");
         lista.add("CREATE TABLE ReseptiRaaka_aine (resepti_id integer, raaka_aine_id integer, jarjestys integer, maara varchar(10), lisaohje varchar(255), FOREIGN KEY (raaka_aine_id) REFERENCES Raaka_aine(id), FOREIGN KEY (resepti_id) REFERENCES Resepti(id));");
-    
-
+        lista.add("CREATE TABLE Kategoria (id integer PRIMARY KEY, nimi varchar(255));");
+        lista.add("CREATE TABLE ReseptiKategoria (resepti_id integer, kategoria_id integer, FOREIGN KEY (kategoria_id) REFERENCES Kategoria(id), FOREIGN KEY (resepti_id) REFERENCES Resepti(id));");
         return lista;
     }
 
