67269f7df7df4c5e47da2ab3e0e5ac82cbc81681
Sini Tenhunen <sini.tenhunen@helsinki.fi>
2018-02-23 10:08:07 +0200

    lisätty raakaAinepaketin luokkiin sisältöä

--- src/main/java/tikape/runko/raakaAine/RaakaAineController.java
+++ src/main/java/tikape/runko/raakaAine/RaakaAineController.java
@@ -1,6 +1,83 @@
-
 package tikape.runko.raakaAine;
 
+import tikape.runko.util.ViewUtil;
+import tikape.runko.database.Database;
+
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import spark.*;
+import tikape.runko.recipe.Recipe;
+import tikape.runko.recipe.RecipeDao;
+import tikape.runko.util.Path;
+
 public class RaakaAineController {
+
+    public Database database;
+    public RaakaAineDao raakaAineDao;
+
+    public RaakaAineController(Database database) {
+        this.database = database;
+        this.raakaAineDao = new RaakaAineDao(this.database);
+    }
+
+    public Route fetchAllRaakaAineet = (Request request, Response response) -> {
+
+        Map<String, Object> model = new HashMap<>();
+        List<RaakaAine> raakaAineet = this.raakaAineDao.findAll(); //Haetaan kaikki raakaAineet
+        model.put("raakaAineet", raakaAineet); //Tallennetaan kaikki reseptit hakusanalle "raakaAineet"
+
+        // mitähän tähän pitäisi tulla
+        model.put("RECIPES", Path.Web.RECIPES);
+        model.put("etusivu_url", Path.Web.INDEX); //Lähetetää avaimlla "etusivu_url" URL etusivulle. Tällä tavalla jos vaihdetaan etusivun URL osoitetta, sitä ei erikseen tarvitse vaihtaa myös täällä
+        //mikä sivu pitäisi palauttaa?
+        return ViewUtil.render(model, Path.Template.RECIPES_ALL); //Palautetaan sivu RECIPES_ALL, joka viittaa html tiedostoon recipe/all.html. Viittaus löytyy Path.java tiedostosta
+    };
+
+    // onko näillä tässä alla enää käyttöä?
     
+    
+    
+    
+//    public Route fetchOneRecipe = (Request request, Response response) -> {
+//
+//        Map<String, Object> model = new HashMap<>();
+//        model.put("resepti", this.recipeDao.findOne(Integer.parseInt(request.params(":id")))); //Napataan Requestin mukana tullut id ja etsitään sitä vastaava resepti
+//
+//        model.put("INDEX", Path.Web.INDEX);
+//
+//        return ViewUtil.render(model, Path.Template.RECIPE);
+//    };
+//
+//    public Route serveAddOneRecipePage = (Request request, Response response) -> {
+//        Map<String, Object> model = new HashMap<>();
+//        model.put("request", request); //Ei haluta palauttaa mitään mallia, mutta koska methodi sitä vaatii niin palautetaan jotain turhaa
+//
+//        model.put("lisää_resepti", Path.Api.ADD_RECIPE);
+//
+//        return ViewUtil.render(model, Path.Template.RECIPE_ADD);
+//    };
+//
+//    public Route serveIndexPage = (Request request, Response reponse) -> {
+//        //Tämä palauttaa kotisivun home.html Tämä methodi pitäisi olla paikassa tikape/runko/index/IndexController.java, mutta en jaksanut tehdä sitä vielä
+//        Map<String, Object> model = new HashMap<>();
+//        model.put("request", request); //Ei haluta palauttaa mitään mallia, mutta koska methodi sitä vaatii niin palautetaan jotain turhaa
+//
+//        model.put("RECIPES", Path.Web.RECIPES);
+//        model.put("lisää_resepti_sivu", Path.Web.ADD_RECIPE);
+//
+//        return ViewUtil.render(model, Path.Template.INDEX);
+//    };
+//
+//    public Route addOneRecipe = (Request request, Response response) -> {
+//        String nimi = request.queryParams("nimi"); //Napataan pyynnön mukana tullut arvo, joka löytyy avaimella "nimi"
+//        Double luokka = Double.parseDouble(request.queryParams("luokka"));
+//        Integer valmistusaika = Integer.parseInt(request.queryParams("valmistusaika"));
+//
+//        this.recipeDao.addOne(nimi, luokka, valmistusaika);
+//
+//        response.redirect(Path.Web.RECIPES); //Uudelleen ohjataan käyttäjä
+//        return ""; //Tämä ei ikinä toteudu
+//    };
+
 }
--- src/main/java/tikape/runko/raakaAine/RaakaAineDao.java
+++ src/main/java/tikape/runko/raakaAine/RaakaAineDao.java
@@ -1,5 +1,90 @@
 package tikape.runko.raakaAine;
 
-public class RaakaAineDao {
-    
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import tikape.runko.database.Database;
+import tikape.runko.raakaAine.RaakaAine;
+import tikape.runko.util.Dao;
+
+public class RaakaAineDao implements Dao<RaakaAine, Integer> {
+
+    private Database database;
+
+    public RaakaAineDao(Database database) {
+        this.database = database;
+    }
+
+    @Override
+    public RaakaAine findOne(Integer key) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM RaakaAine WHERE id = ?");
+        stmt.setObject(1, key);
+
+        ResultSet rs = stmt.executeQuery();
+        boolean hasOne = rs.next();
+        if (!hasOne) {
+            return null;
+        }
+
+        Integer id = rs.getInt("id");
+        String nimi = rs.getString("nimi");
+
+        RaakaAine r = new RaakaAine(id, nimi);
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return r;
+    }
+
+    @Override
+    public List<RaakaAine> findAll() throws SQLException {
+
+        Connection connection = this.database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM RaakaAine");
+
+        ResultSet rs = stmt.executeQuery();
+        List<RaakaAine> raakaAineet = new ArrayList<>();
+        while (rs.next()) {
+            Integer id = rs.getInt("id");
+            String nimi = rs.getString("nimi");
+
+            raakaAineet.add(new RaakaAine(id, nimi));
+        }
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return raakaAineet;
+    }
+
+    public void addOne(String nimi) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("INSERT INTO RaakaAine(nimi) VALUES (?);");
+
+        stmt.setString(1, nimi);
+
+        stmt.execute();
+        stmt.close();
+        connection.close();
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("DELETE FROM RaakaAine WHERE id = ?");
+
+        stmt.setInt(1, key);
+        stmt.executeUpdate();
+
+        stmt.close();
+        connection.close();
+    }
+
 }
