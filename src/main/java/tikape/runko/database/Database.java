28766bc89d33c8c47ff4caa2baac933ff9188d6f
Mikko <32343973+pajaz@users.noreply.github.com>
2018-02-26 17:49:47 +0200

    postgresql test

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -47,10 +47,10 @@ public class Database {
         ArrayList<String> lista = new ArrayList<>();
 
         // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
-        lista.add("CREATE TABLE Resepti (id SERIAL PRIMARY KEY, nimi varchar(255), valmistusaika integer, ohje varchar(1024));");
-        lista.add("CREATE TABLE Raaka_aine (id SERIAL PRIMARY KEY, nimi varchar(255), UNIQUE(nimi));");
+        lista.add("CREATE TABLE Resepti (id SERIAL PRIMARY KEY NOT NULL, nimi varchar(255), valmistusaika integer, ohje varchar(1024));");
+        lista.add("CREATE TABLE Raaka_aine (id SERIAL PRIMARY KEY NOT NULL, nimi varchar(255), UNIQUE(nimi));");
         lista.add("CREATE TABLE ReseptiRaaka_aine (resepti_id integer, raaka_aine_id integer, jarjestys integer, maara varchar(255), FOREIGN KEY (raaka_aine_id) REFERENCES Raaka_aine(id), FOREIGN KEY (resepti_id) REFERENCES Resepti(id));");
-        lista.add("CREATE TABLE Kategoria (id SERIAL PRIMARY KEY, nimi varchar(255));");
+        lista.add("CREATE TABLE Kategoria (id SERIAL PRIMARY KEY NOT NULL, nimi varchar(255));");
         lista.add("CREATE TABLE ReseptiKategoria (resepti_id integer, kategoria_id integer, FOREIGN KEY (kategoria_id) REFERENCES Kategoria(id), FOREIGN KEY (resepti_id) REFERENCES Resepti(id));");
         return lista;
     }
