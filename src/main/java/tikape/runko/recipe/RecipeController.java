0e194fcf219cf2fac29062500c8ad61f1373e842
Mikko <32343973+pajaz@users.noreply.github.com>
2018-02-26 14:40:54 +0200

    recipecontroller lisätty kategoria -ehtolause

--- src/main/java/tikape/runko/recipe/RecipeController.java
+++ src/main/java/tikape/runko/recipe/RecipeController.java
@@ -12,40 +12,38 @@ import tikape.runko.ingredient.Ingredient;
 import tikape.runko.category.*;
 import tikape.runko.util.Path;
 
-
 public class RecipeController {
-    
+
     public Database database;
     public RecipeDao recipeDao;
-    
-    public RecipeController(Database database){
+
+    public RecipeController(Database database) {
         this.database = database;
         this.recipeDao = new RecipeDao(this.database);
     }
-    
+
     public Route fetchAllRecipes = (Request request, Response response) -> {
-        
+
         Map<String, Object> model = new HashMap<>();
         List<Recipe> reseptit = this.recipeDao.findAll(); //Haetaan kaikki reseptit
         model.put("reseptit", reseptit); //Tallennetaan kaikki reseptit hakusanalle "reseptit"
-        
-        
+
         model.put("RECIPES", Path.Web.RECIPES);
         model.put("etusivu_url", Path.Web.INDEX); //Lähetetää avaimlla "etusivu_url" URL etusivulle. Tällä tavalla jos vaihdetaan etusivun URL osoitetta, sitä ei erikseen tarvitse vaihtaa myös täällä
-        
+
         return ViewUtil.render(model, Path.Template.RECIPES_ALL); //Palautetaan sivu RECIPES_ALL, joka viittaa html tiedostoon recipe/all.html. Viittaus löytyy Path.java tiedostosta
     };
-    
+
     public Route fetchOneRecipe = (Request request, Response response) -> {
-        
+
         Map<String, Object> model = new HashMap<>();
         model.put("resepti", this.recipeDao.findOne(Integer.parseInt(request.params(":id")))); //Napataan Requestin mukana tullut id ja etsitään sitä vastaava resepti
-        
+
         model.put("INDEX", Path.Web.INDEX);
-        
+
         return ViewUtil.render(model, Path.Template.RECIPE);
     };
-    
+
     public Route serveAddOneRecipePage = (Request request, Response response) -> {
         Map<String, Object> model = new HashMap<>();
         List<Recipe> reseptit = this.recipeDao.findAll();
@@ -53,7 +51,7 @@ public class RecipeController {
         //Pitäisikö RecipeControllerille antaa itselleen IngredientController vai toteuttaa jotenkin muuten?
         List<Ingredient> raaka_aineet = Main.ingredientController.ingredientDao.findAll();
         List<Category> kategoriat = Main.categoryDao.findAll();
-        
+
         model.put("request", request); //Ei haluta palauttaa mitään mallia, mutta koska methodi sitä vaatii niin palautetaan jotain turhaa
         model.put("reseptit", reseptit);
         model.put("raaka_aineet", raaka_aineet);
@@ -62,52 +60,49 @@ public class RecipeController {
         model.put("kategoriat", kategoriat);
         //model.put("lisää_reseptiin_raaka_aine", Path.Api.??);
         model.put("lisää_raaka_aine", Path.Api.ADD_INGREDIENT);
-        
+
         return ViewUtil.render(model, Path.Template.RECIPE_ADD);
     };
-    
+
     public Route serveIndexPage = (Request request, Response reponse) -> {
         //Tämä palauttaa kotisivun home.html Tämä methodi pitäisi olla paikassa tikape/runko/index/IndexController.java, mutta en jaksanut tehdä sitä vielä
         Map<String, Object> model = new HashMap<>();
         model.put("request", request); //Ei haluta palauttaa mitään mallia, mutta koska methodi sitä vaatii niin palautetaan jotain turhaa
-        
+
         model.put("RECIPES", Path.Web.RECIPES);
         model.put("lisää_resepti_sivu", Path.Web.ADD_RECIPE);
         model.put("lisää_raaka_aine_sivu", Path.Web.ADD_INGREDIENT);
         return ViewUtil.render(model, Path.Template.INDEX);
     };
-    
+
     public Route addOneRecipe = (Request request, Response response) -> {
         String nimi = request.queryParams("nimi"); //Napataan pyynnön mukana tullut arvo, joka löytyy avaimella "nimi"
         Integer valmistusaika = Integer.parseInt(request.queryParams("valmistusaika"));
         String ohje = request.queryParams("ohje");
         String[] raaka_aineet = request.queryParamsValues("raaka-aine");
         String[] maarat = request.queryParamsValues("maara");
-        String[] kategoriat = request.queryParamsValues("kategoria");
-        
+
+        if (request.queryParamsValues("kategoria") != null) {
+            String[] kategoriat = request.queryParamsValues("kategoria");
+            for (String kategoria : kategoriat) {
+                System.out.println("KATEGORIA: " + kategoria);
+            }
+        }
         System.out.println("NIMI: " + nimi);
         System.out.println("VALMISTUSAIKA: " + valmistusaika);
         System.out.println("OHJE: " + ohje);
-        
-        
+
         System.out.println("SEURAAVAKSI LISÄTÄÄN RESEPTI");
         Integer recipe_id = this.recipeDao.addOne(nimi, valmistusaika, ohje);
         System.out.println("RESEPTI LISÄTTY");
-        
-        for(int x = 0; x < raaka_aineet.length; x++){
+
+        for (int x = 0; x < raaka_aineet.length; x++) {
             Integer ingredient_id = Main.ingredientController.ingredientDao.addOne(raaka_aineet[x]);
             Main.recipeIngredientDao.addOne(recipe_id, ingredient_id, x, maarat[x]);
         }
-        
-        for(String kategoria : kategoriat){
-            System.out.println("KATEGORIA: " + kategoria);
-        }
-        
-        
-        
-        
+
         response.redirect(Path.Web.RECIPES); //Uudelleen ohjataan käyttäjä
-        return""; //Tämä ei ikinä toteudu
+        return ""; //Tämä ei ikinä toteudu
     };
-    
+
 }
